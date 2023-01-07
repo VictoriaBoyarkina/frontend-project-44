0b0c74caa24835b3e89ba12db01079714d5d5cd9
Victoria <victoriyaboyarkina@gmail.com>
2023-01-07 09:45:36 +0700

    add cli.js in brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
+import readlineSync from 'readline-sync';
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('Your answer: ');
