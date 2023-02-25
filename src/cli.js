1e18a99d8424155889bfeacf63bf1d7f0ea43710
Victoria <victoriyaboyarkina@gmail.com>
2023-02-25 22:14:16 +0700

    src

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@
 import readlineSync from 'readline-sync';
 
 function brainGames() {
-  console.log('Welcometo the Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ')
   console.log(`Hello, ${name}!`)
 }
